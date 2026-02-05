c45c52026c5ae54412e4ada098825449aaef3561
DmitryVD <d.dumsky@stack.net>
2026-02-05 09:31:05 +0300

    add AI recomendation for COS issue

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ def require_token(f):
 
 app=Flask(__name__)
 
+# Allow specific origin
+CORS(app, origins=['http://localhost:5500'])  # Replace with your local app port
+
 #products = [
 #        {"id": 1, "name": "Keyboard", "price": 49.99},
 #        {"id": 2, "name": "Mouse", "price": 29.99}
